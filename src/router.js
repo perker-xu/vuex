96db8e9a6313b75e00e2fb6c4402cd2bd83f9be5
perker-xu <perker168@163.com>
2019-03-28 16:59:28 +0800

    初始化

--- src/router.js
+++ src/router.js
@@ -6,7 +6,7 @@ Vue.use(Router)
 
 export default new Router({
   routes: [
-    {
+    /* {
       path: '/',
       name: 'home',
       component: Home
@@ -17,7 +17,7 @@ export default new Router({
       // route level code-splitting
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
-    }
+      component: () => import('./views/About.vue')
+    } */
   ]
 })
